0f7a868c9a5a12d31f6f132498935b9b18f189f3
BenDavies1218 <148213274+BenDavies1218@users.noreply.github.com>
2024-08-11 00:09:56 +1000

    feat: created the view page for the tournament

--- src/pages/TournamentPage.jsx
+++ src/pages/TournamentPage.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import DisplayTournamentStats from "../components/DisplayTournamentStats";
 const TournamentPage = () => {
   return (
@@ -9,3 +10,25 @@ const TournamentPage = () => {
 };
 
 export default TournamentPage;
+=======
+import DisplayTournamentStats from "../components/DisplayTournamentStats";
+import Navbar from "../components/Navbar";
+const TournamentPage = () => {
+  return (
+    <>
+      <div className="bg-black">
+        <div className="relative z-20">
+          <Navbar />
+          <div className="overflow-hidden">
+            <div className="flex flex-col container mx-auto h-full">
+              <DisplayTournamentStats />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TournamentPage;
+>>>>>>> Stashed changes
